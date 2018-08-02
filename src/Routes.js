18830d09ac20d427621bddb324e24d7b36d831cb
ShaivyT <shaivysh@gmail.com>
2018-08-02 02:42:24 -0400

    Added Routes, RootReducer, History, Store

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {Route} from 'react-router-dom';
+
+
+export default class Route extends Component{
+
+    render() {
+
+        return (
+            <div>
+            </div>
+        )
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Store from './store';
+import Routes from './Routes';
+import {Router} from 'react-router';
+import {Provider} from 'react-redux';
+import history from './History';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 
-ReactDOM.render("Hello World", document.getElementById('root'));
+
+
+ReactDOM.render(
+    <Provider store={Store}>
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
