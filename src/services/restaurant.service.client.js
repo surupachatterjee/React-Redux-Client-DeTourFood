f6e07e69cf65cde9df955fb7463ee8e7597cb286
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-08-05 23:15:36 -0400

    changes to service file

--- src/services/restaurant.service.client.js
+++ src/services/restaurant.service.client.js
@@ -1,15 +1,25 @@
-let _singleton = Symbol();
+
 
 const RESTAURANT_URL ="https://developers.zomato.com/api/v2.1";
 var cityDetails;
 
+let _singleton = Symbol();
 class RestaurantService{
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Singleton!!!');
+    }
+    static get instance() {
+        if(!this[_singleton])
+            this[_singleton] = new RestaurantService(_singleton);
+        return this[_singleton]
+    }
 
     findLocationDetailsByCity(cityName){
        return fetch(RESTAURANT_URL + "/locations?query=" + cityName,{
            headers:{
-               Accept: 'application/json',
-               userKey: ''//key value here
+               'Accept': 'application/json',
+               'user-key': '8f387705dbb342d6fe530909e541b0dd'//key value here
            }
        }).then(function (response) {
            console.log(response);
@@ -21,22 +31,39 @@ class RestaurantService{
     findAllRestaurants(cityName){
         this.findLocationDetailsByCity(cityName)
             .then(function (response) {
+                let fetchedLoc = response.location_suggestions[0];
+                console.log(fetchedLoc);
                 return fetch(RESTAURANT_URL + "/search?q="+cityName
-                    +"&lat="+response.latitude
-                    +"&lon="+response.longitude
-                    +"&entity_id="+response.entity_id
+                    +"&lat="+fetchedLoc.latitude
+                    +"&lon="+fetchedLoc.longitude
+                    +"&entity_id="+fetchedLoc.entity_id
                     +"&sort=rating"
                     +"&count=150",{
                     headers:{
-                        Accept: 'application/json',
-                        userKey: ''//key value here
+                        'Accept': 'application/json',
+                        'user-key': '8f387705dbb342d6fe530909e541b0dd'//key value here
                     }
+                }).then(function (response) {
+                    return response.json();
                 })
-                    .then(function (response) {
-                        return response.json();
-                    })
             })
     }
+
+   /* findRestaurantsByLoc(fetchedLoc,cityName){
+        return fetch(RESTAURANT_URL + "/search?q="+cityName
+            +"&lat="+fetchedLoc.latitude
+            +"&lon="+fetchedLoc.longitude
+            +"&entity_id="+fetchedLoc.entity_id
+            +"&sort=rating"
+            +"&count=150",{
+            headers:{
+                'Accept': 'application/json',
+                'user-key': '8f387705dbb342d6fe530909e541b0dd'//key value here
+            }
+        }).then(function (response) {
+            return response.json();
+        })
+    }*/
 }
 
 export default RestaurantService;
