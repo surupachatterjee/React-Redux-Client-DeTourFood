506f05c50dd3249fb80b747a3a8bfa0754977626
ShaivyT <shaivysh@gmail.com>
2018-08-07 20:08:20 -0400

    modifying the restaurant results

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -9,10 +9,14 @@ class SearchComponent extends React.PureComponent {
                 {this.props.restaurants.restaurants && this.props.restaurants.restaurants.map(restaurant => {
                     console.log(restaurant);
                     return (
-                        <div className="card" key={restaurant.restaurant.id}>
+                        <div className="card w-50" key={restaurant.restaurant.id}>
                             <div className="card-body" key={restaurant.restaurant.id}>
                                 <h4 className="card-title">Restaurant Name: {restaurant.restaurant.name}</h4>
                                 <h4 className="card-title">Cuisines: {restaurant.restaurant.cuisines}</h4>
+                                    <h4 className="card-title">
+                                        Featured Image: <img className="card-img-top"
+                                                             src={restaurant.restaurant.featured_image}/>
+                                    </h4>
                             </div>
                         </div>
                     );
