5cf1179f6b1ea970806f03e5ee27af8cafd9e706
Surupa Tushar Chatterjee <40750218+surupachatterjee@users.noreply.github.com>
2018-09-07 15:14:28 -0400

    API Endpoint change

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import * as constants from "../constants";
 import history from '../History';
 import UserService from "../services/user.service.client";
 import RestaurantService from '../services/restaurant.service.client';
-const RESTAURANT_URL ="https://detour-food-backend.herokuapp.com/api/restaurant";
+const RESTAURANT_URL ="https://detour-food-backend-stc.herokuapp.com/api/restaurant";
 //import MenuService from '../services/menu.service.client';
 const MENU_URL = 'https://detour-food-backend-stc.herokuapp.com/api/menu';
 const ORDER_URL = 'https://detour-food-backend-stc.herokuapp.com/api/order';
